116740627e4be2d764089c522d7c5eff3dc1211f
Mohsen Mozafari <73017441+MohsenMozaffary@users.noreply.github.com>
2025-04-12 23:44:22 -0400

    Update web_loader.py
    WebLoader updated to take list of URLs

--- WebScraping/web_loader.py
+++ WebScraping/web_loader.py
@@ -1,32 +1,35 @@
-from langchain.document_loaders import RecursiveUrlLoader, WebBaseLoader
+from langchain.document_loaders import WebBaseLoader, RecursiveUrlLoader
 
-class webScraper():
 
-    def __init__(self, mode ="base"):
+class WebLoader:
 
-        self.mode = mode
+    def __init__(self, depth =2, n_selection = 5):
+
+        self.depth = depth
+        self.n_selection = n_selection
+
+    def recusrsive_load_web(self, url):
+
+        loader = RecursiveUrlLoader(
+                                    url,
+                                    max_depth=self.depth)
+        
+        docs = loader.load()
+        docs = [doc for doc in docs if 'title' in doc.metadata]
+        titles = [doc.metadata['title'] for doc in docs]
+
+        if self.n_selection > len(titles):
+            self.n_selection = len(titles)
+            print("n_selection is changed to {}".format(len(titles)))
+
+        return titles, docs
+    
 
     def base_loader(self, url):
 
         loader = WebBaseLoader(url)
         docs = loader.load()
 
-        return docs
-    
-    def recursive(self, url, depth = 2):
-        
-        loader = RecursiveUrlLoader(url, max_depth = depth)
-        docs = loader.load()
+        contents = [doc.page_content for doc in docs]
 
-        return docs
-
-    def scrape_url(self, url, max_depth = 2):
-
-        if self.mode == "base":
-            docs = self.base_loader(url)
-        elif self.mode == "recursive":
-            docs = self.recursive(url, depth = max_depth)
-
-        return docs
-
-
+        return contents
